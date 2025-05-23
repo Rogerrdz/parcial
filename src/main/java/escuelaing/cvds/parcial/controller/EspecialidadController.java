6320dec92e1777e56612cb7f815b127caac65d56
unknown <roger.rodriguez-a@labinfo.is.escuelaing.edu.co>
2025-05-23 17:07:04 -0500

    Parcial

--- src/main/java/escuelaing/cvds/parcial/controller/EspecialidadController.java
+++ src/main/java/escuelaing/cvds/parcial/controller/EspecialidadController.java
@@ -2,4 +2,26 @@ package escuelaing.cvds.parcial.controller;
 
 import escuelaing.cvds.parcial.model.Especialidad;
 import escuelaing.cvds.parcial.service.EspecialidadService;
+import org.springframework.web.dind.anotation.*;
 
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/especialidades")
+public class EspecialidadController {
+    private final EspecialidadService service;
+
+    public EspecialidadController(EspecialidadService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Especialidad> listar() {
+        return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    public Especialidad obtener(@Pathvariable String id){
+        return service.obtenerPorId(id);
+    }
+}
